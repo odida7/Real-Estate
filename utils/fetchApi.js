3d65844217308f6212eb34094bf929703c812c75
odida7 <danielodida@yahoo.com>
2023-03-31 17:26:25 +0300

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+
+export const baseUrl = 'https://bayut.p.rapidapi.com';
+
+
+ export const fetchApi = async (url) => {
+    const {data} = await axios.get((url), {
+        headers: {
+          'X-RapidAPI-Key': '4066b6dcccmsh494acf267eb626dp1f356ajsn0f2a08cc8144',
+          'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+        }
+    })
+    return data;
+ }
